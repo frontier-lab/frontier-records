94cb4dee43175c8fc867479175a1e9b69cde17bb
jaeeun.yoo/PAYCO컨텐츠개발팀/NP <jaeeun.yoo@nhnent.com>
2019-01-10 12:34:33 +0900

    코드 스타일 적용

--- front/src/main/java/com/frontier/records/front/configuration/WebMvcConfig.java
+++ front/src/main/java/com/frontier/records/front/configuration/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.frontier.records.front.configuration;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
@@ -11,48 +12,48 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
-import java.util.List;
-
 
 @Configuration
 @EnableWebMvc
+@SuppressWarnings("CheckStyle")
 public class WebMvcConfig implements WebMvcConfigurer {
-	
-	@Bean
-	public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
-		return new DeviceResolverHandlerInterceptor();
-	}
-	
-	@Bean
-	public DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver() {
-		return new DeviceHandlerMethodArgumentResolver();
-	}
-	
-	@Bean
-	public LiteDeviceDelegatingViewResolver liteDeviceAwareViewResolver() {
-		FreeMarkerViewResolver delegate = new FreeMarkerViewResolver();
-		delegate.setPrefix("");
-		delegate.setSuffix(".ftl");
-		
-		LiteDeviceDelegatingViewResolver resolver = new LiteDeviceDelegatingViewResolver(delegate);
-		resolver.setMobilePrefix("");
-		resolver.setMobileSuffix("/mobile");
-		resolver.setNormalPrefix("");
-		resolver.setNormalSuffix("/pc");
-		resolver.setTabletPrefix("");
-		resolver.setTabletSuffix("/mobile");
-		
-		return resolver;
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(deviceResolverHandlerInterceptor());
-	}
-	
-	@Override
-	public void addArgumentResolvers(
-		List<HandlerMethodArgumentResolver> argumentResolvers) {
-		argumentResolvers.add(deviceHandlerMethodArgumentResolver());
-	}
+
+    @Bean
+    public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
+        return new DeviceResolverHandlerInterceptor();
+    }
+
+    @Bean
+    public DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver() {
+        return new DeviceHandlerMethodArgumentResolver();
+    }
+
+
+    @Bean
+    public LiteDeviceDelegatingViewResolver liteDeviceAwareViewResolver() {
+        FreeMarkerViewResolver delegate = new FreeMarkerViewResolver();
+        delegate.setPrefix("");
+        delegate.setSuffix(".ftl");
+
+        LiteDeviceDelegatingViewResolver resolver = new LiteDeviceDelegatingViewResolver(delegate);
+        resolver.setMobilePrefix("");
+        resolver.setMobileSuffix("/mobile");
+        resolver.setNormalPrefix("");
+        resolver.setNormalSuffix("/pc");
+        resolver.setTabletPrefix("");
+        resolver.setTabletSuffix("/mobile");
+
+        return resolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(deviceResolverHandlerInterceptor());
+    }
+
+    @Override
+    public void addArgumentResolvers(
+        List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+    }
 }
