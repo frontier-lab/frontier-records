9e6b36b09b37d77839edc0f0fbf5867b11997705
GwiYeong <jgy0726@gmail.com>
2019-01-14 23:36:52 +0900

    merge main controller

--- front/src/main/java/com/frontier/records/front/main/presentation/MainController.java
+++ front/src/main/java/com/frontier/records/front/main/presentation/MainController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @GetMapping("**")
+    public String main() {
+        return "index";
+    }
+
     @GetMapping("/login")
     private String login() {
         return "login";
     }
+
+
 }
