676ad39ef43afd150e015ebf02bec0e33332f793
GwiYeong <jgy0726@gmail.com>
2019-01-27 17:01:55 +0900

    change css file

--- front/src/main/resources/app/src/component/mobile/App.jsx
+++ front/src/main/resources/app/src/component/mobile/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import '../../assets/mobile/css/common.css';
+import '../../assets/mobile/css/common_test.css';
 import '../../assets/mobile/css/member_test.css'
 
 import Router from './router';
